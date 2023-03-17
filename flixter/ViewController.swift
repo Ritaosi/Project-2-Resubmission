c7bf1f77af78fbaf1f4fa21bfc11bc800b134938
Ritaosi <122660915+Ritaosi@users.noreply.github.com>
2023-03-17 17:28:41 -0500

    Add files via upload

---
+++ flixter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Rita Osi on 3/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
